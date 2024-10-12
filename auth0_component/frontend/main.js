79d48505eae7533e4df452da48285294232648b5
danwatco <dan.watco@gmail.com>
2024-10-12 17:14:31 +0100

    Update to check claims on load

--- auth0_component/frontend/main.js
+++ auth0_component/frontend/main.js
@@ -1,8 +1,7 @@
-import { Streamlit } from "streamlit-component-lib"
 import createAuth0Client from '@auth0/auth0-spa-js';
-import Toastify from 'toastify-js'
-import "toastify-js/src/toastify.css"
-import "./style.css"
+import { Streamlit } from "streamlit-component-lib";
+import "toastify-js/src/toastify.css";
+import "./style.css";
 
 const div = document.body.appendChild(document.createElement("div"))
 const button = div.appendChild(document.createElement("button"))
@@ -19,7 +18,7 @@ let domain
 let auth0
 
 const logout = async () => {
-  auth0.logout({returnTo: getOriginUrl()})
+  auth0.logout({ returnTo: getOriginUrl() })
   button.textContent = "Login"
   button.removeEventListener('click', logout)
   button.addEventListener('click', login)
@@ -29,64 +28,73 @@ const login = async () => {
   button.textContent = 'working...'
   console.log('Callback urls set to: ', getOriginUrl())
   auth0 = await createAuth0Client({
-      domain: domain,
-      client_id: client_id,
-      redirect_uri: getOriginUrl(),
-      audience:`https://${domain}/api/v2/`,
-      useRefreshTokens: true,
-      cacheLocation: "localstorage",
-    });
-    try{
-      await auth0.loginWithPopup();
-      errorNode.textContent = ''
-    }
-    catch(err){
-      console.log(err)
-      errorNode.textContent = `Popup blocked, please try again or enable popups` + String.fromCharCode(160)
-      return
-    }
-    const user = await auth0.getUser();
-    console.log(user)
-    console.log({
-      // return getAccessTokenWithPopup({
-        audience:`https://${domain}/api/v2/`,
-        scope: "read:current_user",
-      })
-    let token = false
-    
-    try{
-    token = await auth0.getTokenSilently({
-        // return getAccessTokenWithPopup({
-          audience:`https://${domain}/api/v2/`,
-          // scope: "read:current_user",
-        });
-      }
-      catch(error){
-        if (error.error === 'consent_required' || error.error === 'login_required'){
-          console.log('asking user for permission to their profile')
-           token = await auth0.getTokenWithPopup({
-              audience:`https://${domain}/api/v2/`,
-              scope: "read:current_user",
-            });
-            console.log(token)
-        }
-        else{console.log(error)}
-      }
+    domain: domain,
+    client_id: client_id,
+    redirect_uri: getOriginUrl(),
+    audience: `https://${domain}/api/v2/`,
+    useRefreshTokens: true,
+    cacheLocation: "localstorage",
+  });
+  try {
+    await auth0.loginWithPopup();
+    errorNode.textContent = ''
+  }
+  catch (err) {
+    console.log(err)
+    errorNode.textContent = `Popup blocked, please try again or enable popups` + String.fromCharCode(160)
+    return
+  }
+  const user = await auth0.getUser();
+  console.log(user)
+  console.log({
+    // return getAccessTokenWithPopup({
+    audience: `https://${domain}/api/v2/`,
+    scope: "read:current_user",
+  })
+  let token = false
 
-    let userCopy = JSON.parse(JSON.stringify(user));
-    userCopy.token = token
-    console.log(userCopy);
-    Streamlit.setComponentValue(userCopy)
-    button.textContent = "Logout"
-    button.removeEventListener('click', login)
-    button.addEventListener('click', logout)
+  try {
+    token = await auth0.getTokenSilently({
+      // return getAccessTokenWithPopup({
+      audience: `https://${domain}/api/v2/`,
+      // scope: "read:current_user",
+    });
+  }
+  catch (error) {
+    if (error.error === 'consent_required' || error.error === 'login_required') {
+      console.log('asking user for permission to their profile')
+      token = await auth0.getTokenWithPopup({
+        audience: `https://${domain}/api/v2/`,
+        scope: "read:current_user",
+      });
+      console.log(token)
+    }
+    else { console.log(error) }
+  }
+
+  let userCopy = JSON.parse(JSON.stringify(user));
+  userCopy.token = token
+  console.log(userCopy);
+  Streamlit.setComponentValue(userCopy)
+  button.textContent = "Logout"
+  button.removeEventListener('click', login)
+  button.addEventListener('click', logout)
 }
 
-button.onclick = login
+const claims = await auth0.checkSession();
+
+if (claims) {
+  button.textContent = "Logout"
+  button.removeEventListener('click', login)
+  button.addEventListener('click', logout)
+} else {
+  button.onclick = login
+}
+
 
 function onRender(event) {
   const data = event.detail
-  
+
   client_id = data.args["client_id"]
   domain = data.args["domain"]
 
